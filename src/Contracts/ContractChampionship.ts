1cec66f9ebd075fcb7fff96ec23e38d825aafef3
Rafael Santos <santosrafaell187@gmail.com>
2022-07-18 10:36:07 -0300

    feat: create new contract for championsship metthod

--- src/Contracts/ContractChampionship.ts
+++ src/Contracts/ContractChampionship.ts
@@ -1,1 +1,29 @@
-export default interface ContractChampionship {}
+type PropertyField = number | string;
+
+type ChampionshipType = {
+  name: string;
+
+  position: PropertyField;
+
+  points: PropertyField;
+
+  imageUrl: string;
+
+  matches: PropertyField;
+
+  winners: PropertyField;
+
+  draws: PropertyField;
+
+  loss: PropertyField;
+
+  goalsScored: PropertyField;
+
+  goalsTaken: PropertyField;
+
+  goalsDiference: PropertyField;
+};
+
+export default interface ContractChampionship {
+  showChampionshipTable: () => Promise<ChampionshipType[]>;
+}
